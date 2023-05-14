f7e0db2e528da52855f349e6cf1e956890b3fd07
边圣陶 <>
2023-05-15 01:51:31 +0800

    230515 update

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/5/15 01:20
+@Auth ： 边圣陶
+@File ：config.py
+@IDE ：PyCharm
+@Email ：bian.shengtao@gmail.com
+"""
+# your username
+your_username = "your_username"
+# your password
+your_password = "your_password"
+# chromedriver path
+chromedriver_dir = "path/to/your/chromedriver"
